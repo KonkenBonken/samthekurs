df1be3c9f775a26e49c78ea9736dd7d16d1a946d
KonkenBonken <konrad05pettersson@gmail.com>
2024-03-30 17:21:50 +0100

    create front page

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-import scss from './styles/_main.module.scss';
-
 export default function Main() {
-  return (
-    <h1 className={scss.header}>
-      Hello World
-    </h1>
-  );
+  return (<>
+    <header>
+      <div>
+        <h2>"Att vara kreatör är att inspirera"</h2>
+        <p>- Sam Pettersson</p>
+      </div>
+      <img src="/motiv.png" alt="Sam Pettersson" />
+    </header>
+    <main>
+      <h1>SamTheKurs</h1>
+      <h4>Lär dig allt om hur man lyckas på TikTok</h4>
+      <h5>Köp min kurs för endast</h5>
+      <div><i>899:-</i><b>699:-</b></div>
+      <button>Köp</button>
+    </main>
+  </>);
 }
